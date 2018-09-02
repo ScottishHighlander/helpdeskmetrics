b0668c89458ed18f06381fb003c6c16080979aa7
Alexander Chan <singjai@gmail.com>
2018-09-01 19:57:57 -0700

    Update 3-explore-agencies.R

--- 3-explore-agencies.R
+++ 3-explore-agencies.R
@@ -2,3 +2,6 @@ library(dplyr)
 
 # create data frame for unique tickets
 agencies <- as.data.frame(table(metrics.data$Facility))
+
+# what is the difference between this and the "3-explore.R" file?  I'll probably find out if I see the actual dataset but what agencies
+# is this speaking of?
